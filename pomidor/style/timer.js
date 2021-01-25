0c68db5589f7aebae80bb574166f02d1d71c5c04
Arachnoz <arachnoz@idb.net.ru>
2021-01-25 13:41:39 +0300

    Update timer.js

--- pomidor/style/timer.js
+++ pomidor/style/timer.js
@@ -13,7 +13,7 @@ var now = new Date().getTime();
 var distance = countDownDate - now;
 // подсчёт calculations for days, hours, minutes and seconds
 var days = distance * 24;
-var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + days;
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 // вывод результатов с подставлением нулей
