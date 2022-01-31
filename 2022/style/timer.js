a51e28cacf546399ff3fbc036c7ffe387acca9a2
Arachnoz <arachnoz@idb.net.ru>
2022-02-01 02:31:16 +0300

    Add files via upload

---
+++ 2022/style/timer.js
@@ -0,0 +1,24 @@
+// подставляем нули если число меньше 10
+function pad(n) {
+if (n < 10) return "0" + n;
+return n;
+}
+// установка даты таймера
+var countDownDate = new Date("Feb 04, 2022 23:59:00").getTime();
+// ежесекундное обновление
+var x = setInterval(function() {
+// сегодняшние дата и время
+var now = new Date().getTime();
+// разность между таймером и текущим временем
+var distance = countDownDate - now;
+// подсчёт calculations for days, hours, minutes and seconds
+var hours = Math.floor(distance / (1000 * 60 * 60));
+//var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+// вывод результатов с подставлением нулей
+//document.getElementById("days").innerHTML = pad(days);
+document.getElementById("hours").innerHTML = pad(hours);
+document.getElementById("min").innerHTML = pad(minutes);
+document.getElementById("sec").innerHTML = pad(seconds);
+}, 1000);
