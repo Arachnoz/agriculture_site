8ab1233b35d97a36318d02f8c6e5d780f07d8199
Arachnoz <arachnoz@idb.net.ru>
2022-09-14 14:52:39 +0300

    Update timer.js

--- op/style/timer.js
+++ op/style/timer.js
@@ -4,7 +4,7 @@ if (n < 10) return "0" + n;
 return n;
 }
 // установка даты таймера
-var countDownDate = new Date("Sep 16, 2022 23:59:00").getTime();
+var countDownDate = new Date("Sep 16, 2022 23:30:00").getTime();
 // ежесекундное обновление
 var x = setInterval(function() {
 // сегодняшние дата и время
@@ -13,13 +13,13 @@ var now = new Date().getTime();
 var distance = countDownDate - now;
 // подсчёт calculations for days, hours, minutes and seconds
 // - если нужны дни, а не часы -
-var days = Math.floor(distance / (1000 * 60 * 60 * 24));  
+//var days = Math.floor(distance / (1000 * 60 * 60 * 24));  
 var hours = Math.floor(distance / (1000 * 60 * 60));
 //var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 // вывод результатов с подставлением нулей
-document.getElementById("days").innerHTML = pad(days);
+//document.getElementById("days").innerHTML = pad(days);
 document.getElementById("hours").innerHTML = pad(hours);
 document.getElementById("min").innerHTML = pad(minutes);
 document.getElementById("sec").innerHTML = pad(seconds);
